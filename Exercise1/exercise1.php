3025409c35ce20cd42df7d93ff4827458d537c23
Ben Moeller <benlynnmoeller@ku.edu>
2021-11-18 14:42:43 -0600

    adding exercise 1

---
+++ Exercise1/exercise1.php
@@ -0,0 +1,29 @@
+<?php
+function multiply($x,$y) {
+  $z = $x * $y;
+  return $z;
+}
+
+
+echo "<table>";
+for ($i = 0; $i <= 100; $i++) {
+  echo "<tr>";
+  for($j = 0; $j <= 100; $j++) {
+    if($i == 0) {
+      if($j == 0) {
+        echo "<td></td>";
+      }
+      echo "<td>" . ($j + 1) . "</td>";
+    }
+    else if ($j == 0) {
+      echo "<td>" . ($i) . "</td>";
+    }
+    else {
+      echo "<td>" . multiply($i,$j) . "</td>";
+    }
+  }
+  echo "</tr>";
+}
+echo "</table>";
+
+?>
